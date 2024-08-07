81ce83c644751d2734df868c68e176d7d9369461
alikadkhoda <kadkhoda.rezaali@gmail.com>
2024-08-07 13:07:01 +0430

    introduction added

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Introduction() {
+  return (
+    <div>
+        <div className="container m-auto py-32">
+            <div className="grid grid-cols-1 sm:grid-cols-2 space-y-10 sm:space-y-0 items-center text-slate-900 text-justify">
+                <div className="px-10 ">
+                    <h2 className="font-bold text-4xl">ما به شرکت‌ها کمک می‌کنیم تا محصولات دیجیتالی ایجاد و عرضه کنند</h2>
+                </div>
+                <div className="space-y-4 px-10 text-lg">
+                    <p>برنامه ریزیلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+                    <p>طراحیلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+                    <p>توسعهلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+  )
+}
