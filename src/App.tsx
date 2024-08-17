0da90ce5f80f6e506a16fed438b0e2b3110bde75
alikadkhoda <kadkhoda.rezaali@gmail.com>
2024-08-17 10:49:23 +0430

    Expanding the project and changing the project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import About from "./components/About"
-import Cards from "./components/Cards"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
-import Hero from "./components/Hero"
-import Introduction from "./components/Introduction"
-import Navbar from "./components/Navbar"
-import ProductionProcess from "./components/ProductionProcess"
-import Projects from "./components/Projects"
-import Services from "./components/Services"
-import Slider from "./components/Slider"
+import { RouterProvider } from "react-router-dom"
+import routes from "./routes/routes"
 
 function App() {
 
   return (
     <>
-     <Navbar />
-     <Hero />
-     <Introduction />
-     <Projects />
-     <ProductionProcess />
-     <Services />
-     <About />
-     <Slider />
-     <Cards />
-     <Contact />
-     <Footer />
+    <RouterProvider router={routes} />
     </>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import About from "../components/About";
+import Cards from "../components/Cards";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Introduction from "../components/Introduction";
+import ProductionProcess from "../components/ProductionProcess";
+import Projects from "../components/Projects";
+import Services from "../components/Services";
+import Slider from "../components/Slider";
+
+export default function Home() {
+  return (
+    <div>
+     <Hero />
+     <Introduction />
+     <Projects />
+     <ProductionProcess />
+     <Services />
+     <About />
+     <Slider />
+     <Cards />
+     <Contact />
+    </div>
+  )
+}
